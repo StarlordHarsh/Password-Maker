07828906d7b49755f9695b58c6d93ff67fa85152
Harsh Jaiswal <hj101998@gmail.com>
2019-03-05 19:46:06 +0530

    .

--- src/password/maker/NewJFrame.java
+++ src/password/maker/NewJFrame.java
@@ -29,18 +29,33 @@ public class NewJFrame extends javax.swing.JFrame {
 
         Passlabel = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
-        jCheckBox1 = new javax.swing.JCheckBox();
-        jCheckBox2 = new javax.swing.JCheckBox();
+        Chksmall = new javax.swing.JCheckBox();
+        Chkcap = new javax.swing.JCheckBox();
+        Chknum = new javax.swing.JCheckBox();
+        Chkspec = new javax.swing.JCheckBox();
+        Slider = new javax.swing.JSlider();
+        Labword = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         Passlabel.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
 
-        jCheckBox1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jCheckBox1.setText("Small");
+        Chksmall.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Chksmall.setText("Small");
 
-        jCheckBox2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jCheckBox2.setText("Capital");
+        Chkcap.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Chkcap.setText("Capital");
+        Chkcap.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ChkcapActionPerformed(evt);
+            }
+        });
+
+        Chknum.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Chknum.setText("Numbers");
+
+        Chkspec.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Chkspec.setText("Special Characters");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -48,20 +63,41 @@ public class NewJFrame extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jCheckBox1)
+                .addComponent(Chksmall)
                 .addGap(81, 81, 81)
-                .addComponent(jCheckBox2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(Chkcap)
+                .addGap(81, 81, 81)
+                .addComponent(Chknum)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
+                .addComponent(Chkspec)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jCheckBox1)
-                    .addComponent(jCheckBox2))
+                    .addComponent(Chksmall)
+                    .addComponent(Chkcap)
+                    .addComponent(Chknum)
+                    .addComponent(Chkspec))
                 .addContainerGap(19, Short.MAX_VALUE))
         );
+
+        Slider.setMajorTickSpacing(10);
+        Slider.setMaximum(50);
+        Slider.setMinimum(8);
+        Slider.setPaintLabels(true);
+        Slider.setPaintTicks(true);
+        Slider.setValue(0);
+        Slider.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                SliderStateChanged(evt);
+            }
+        });
+
+        Labword.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        Labword.setText("No. of Words-");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -71,9 +107,12 @@ public class NewJFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(Passlabel)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 428, Short.MAX_VALUE)))
+                        .addComponent(Slider, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(107, 107, 107)
+                        .addComponent(Labword)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -81,13 +120,29 @@ public class NewJFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addComponent(Passlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 209, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(55, 55, 55)
+                        .addComponent(Slider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Labword)
+                        .addGap(44, 44, 44)))
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(167, 167, 167))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void SliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_SliderStateChanged
+        Labword.setText("No. of Words-"+Integer.toString(Slider.getValue()));
+    }//GEN-LAST:event_SliderStateChanged
+
+    private void ChkcapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ChkcapActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ChkcapActionPerformed
 
     /**
      * @param args the command line arguments
@@ -125,9 +180,13 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox Chkcap;
+    private javax.swing.JCheckBox Chknum;
+    private javax.swing.JCheckBox Chksmall;
+    private javax.swing.JCheckBox Chkspec;
+    private javax.swing.JLabel Labword;
     private javax.swing.JTextField Passlabel;
-    private javax.swing.JCheckBox jCheckBox1;
-    private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JSlider Slider;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
